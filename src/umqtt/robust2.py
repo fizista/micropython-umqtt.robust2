2bcf3986aa27b1c66ce1c9120a1eee114c6424ea
Wojciech Banaś <fizista@gmail.com>
2019-12-21 12:15:31 +0100

    umqtt.robust2: First modified version

--- src/umqtt/robust2.py
+++ src/umqtt/robust2.py
@@ -1,43 +1,204 @@
 import utime
-from . import simple
+from . import simple2
 
-class MQTTClient(simple.MQTTClient):
 
-    DELAY = 2
+class MQTTClient(simple2.MQTTClient):
     DEBUG = False
 
-    def delay(self, i):
-        utime.sleep(self.DELAY)
+    # Information whether we store unsent messages with the flag QoS==0 in the queue.
+    KEEP_QOS0 = True
+    # TODO: implement
+    NO_QUEUE_DUPS = True
 
-    def log(self, in_reconnect, e):
+    def __init__(self, *args, **kwargs):
+        "See documentation for `umqtt.simple2.MQTTClient.__init__()`"
+        super().__init__(*args, **kwargs)
+        self.msg_to_send = []  # Queue with list of messages to send
+        self.sub_to_send = []  # Queue with list of subscriptions to send
+        self.msg_to_confirm = {}  # Queue with a list of messages waiting for the server to confirm receipt of the message.
+        self.sub_to_confirm = {}  # A queue with a subscription list waiting for the server to confirm receipt of the subscription.
+        self.conn_issue = None  # We store here if there is a connection problem.
+
+    def set_callback_status(self, f):
+        "See documentation for `umqtt.simple2.MQTTClient.set_callback_status()`"
+        self._cbstat = f
+
+    def cbstat(self, pid, stat):
+        """
+        Captured message statuses affect the queue here.
+
+        stat == 0 - the message goes back to the message queue to be sent
+        stat == 1 or 2 - the message is removed from the queue
+        """
+        try:
+            self._cbstat(pid, stat)
+        except AttributeError:
+            pass
+
+        for data, pids in self.msg_to_confirm.items():
+            if pid in pids:
+                if stat == 0:
+                    self.msg_to_send.append(data)
+                pids.remove(pid)
+                if not pids:
+                    self.msg_to_confirm.pop(data)
+                return
+
+        for data, pids in self.sub_to_confirm.items():
+            if pid in pids:
+                if stat == 0:
+                    self.sub_to_send.append(data)
+                pids.remove(pid)
+                if not pids:
+                    self.sub_to_confirm.pop(data)
+
+    def connect(self, clean_session=True, socket_timeout=-1):
+        """
+        See documentation for `umqtt.simple2.MQTTClient.connect()`.
+
+        If clean_session==True, then the queues are cleared.
+
+        Connection problems are captured and handled by `wait_msg()`
+        """
+        if clean_session:
+            self.msg_to_send[:] = []
+            self.msg_to_confirm.clear()
+        try:
+            out = super().connect(clean_session, socket_timeout)
+            self.conn_issue = None
+            return out
+        except (OSError, simple2.MQTTException) as e:
+            self.conn_issue = (e, 1)
+
+    def log(self, in_place, e):
         if self.DEBUG:
-            if in_reconnect:
-                print("mqtt reconnect: %r" % e)
+            place_str = ('?', 'connect', 'publish', 'subscribe', 'reconnect', 'sendquaue')
+            print("MQTT (%s): %r" % (place_str[in_place], e))
+
+    def reconnect(self, socket_timeout=-1):
+        """
+        The function tries to resume the connection.
+
+        Connection problems are captured and handled by `wait_msg()`
+        """
+        try:
+            out = super().connect(False, socket_timeout=socket_timeout)
+            self.conn_issue = None
+            return out
+        except (OSError, simple2.MQTTException) as e:
+            self.conn_issue = (e, 4)
+
+    def publish(self, topic, msg, retain=False, qos=0, socket_timeout=-1):
+        """
+        See documentation for `umqtt.simple2.MQTTClient.publish()`
+
+        The function tries to send a message. If it fails, the message goes to the message queue for sending.
+
+        The function does not support the `dup` parameter!
+
+        When we have messages with the retain flag set, only one last message with that flag is sent!
+
+        Connection problems are captured and handled by `wait_msg()`
+
+        :return: None od PID for QoS==1 (only if the message is sent immediately, otherwise it returns None)
+        """
+        data = (topic, msg, retain, qos)
+        if retain:
+            # We delete all previous messages for this topic with the retain flag set to True.
+            # Only the last message with this flag is relevant.
+            self.msg_to_send[:] = [m for m in self.msg_to_send if not (topic == m[0] and retain == m[2])]
+        try:
+            out = super().publish(topic, msg, retain, qos, False, socket_timeout)
+            if qos == 1:
+                # We postpone the message in case it is not delivered to the server.
+                # We will delete it when we receive a receipt.
+                self.msg_to_confirm.setdefault(data, []).append(out)
+            return out
+        except (OSError, simple2.MQTTException) as e:
+            self.conn_issue = (e, 2)
+            # If the message cannot be sent, we put it in the queue to try to resend it.
+            if self.KEEP_QOS0 and qos == 0:
+                self.msg_to_send.append(data)
+            elif qos == 1:
+                self.msg_to_send.append(data)
+
+    def subscribe(self, topic, qos=0, socket_timeout=-1):
+        """
+        See documentation for `umqtt.simple2.MQTTClient.subscribe()`
+
+        The function tries to subscribe to the topic. If it fails, the topic subscription goes into the subscription queue.
+
+        Connection problems are captured and handled by `wait_msg()`
+        """
+        data = (topic, qos)
+        # We delete all previous subscriptions for the same topic from the queue.
+        # The most important is the last subscription.
+        self.sub_to_send[:] = [s for s in self.sub_to_send if topic != s[0]]
+        try:
+            out = super().subscribe(topic, qos, socket_timeout)
+            self.sub_to_confirm.setdefault(data, []).append(out)
+            return out
+        except (OSError, simple2.MQTTException) as e:
+            self.conn_issue = (e, 3)
+            # We delete all previous subscriptions for the same topic from the queue.
+            # The most important is the last subscription.
+            self.sub_to_send.append(data)
+
+    def send_queue(self, socket_timeout=-1):
+        """
+        The function tries to send all messages and subscribe to all topics that are in the queue to send.
+
+        :return: True if the queue's empty.
+        :rtype: bool
+        """
+        msg_to_del = []
+        for data in self.msg_to_send:
+            topic, msg, retain, qos = data
+            try:
+                out = super().publish(topic, msg, retain, qos, False, socket_timeout)
+                if qos == 1:
+                    # We postpone the message in case it is not delivered to the server.
+                    # We will delete it when we receive a receipt.
+                    self.msg_to_confirm.setdefault(data, []).append(out)
+                msg_to_del.append(data)
+            except (OSError, simple2.MQTTException) as e:
+                self.conn_issue = (e, 5)
+                return False
+        self.msg_to_send[:] = [m for m in self.msg_to_send if m not in msg_to_del]
+        del (msg_to_del)
+
+        sub_to_del = []
+        for data in self.sub_to_send:
+            topic, qos = data
+            try:
+                out = super().subscribe(topic, qos, socket_timeout)
+                self.sub_to_confirm.setdefault(data, []).append(out)
+                sub_to_del.append(data)
+            except (OSError, simple2.MQTTException) as e:
+                self.conn_issue = (e, 5)
+                return False
+        self.sub_to_send[:] = [s for s in self.sub_to_send if s not in sub_to_del]
+
+        return True
+
+    def wait_msg(self, socket_timeout=None):
+        """
+        See documentation for `umqtt.simple2.MQTTClient.wait_msg()`
+
+        The function tries to subscribe to the topic. If it fails, the topic subscription goes into the subscription queue.
+
+        Connection problems are captured and handled by `wait_msg()`
+        """
+        if self.conn_issue:
+            if type(self.conn_issue) is tuple:
+                conn_issue, issue_place = self.conn_issue
+                self.log(issue_place, conn_issue)
             else:
-                print("mqtt: %r" % e)
+                self.log(0, self.conn_issue)
 
-    def reconnect(self):
-        i = 0
-        while 1:
+            self.reconnect(socket_timeout)
+        else:
             try:
-                return super().connect(False)
-            except OSError as e:
-                self.log(True, e)
-                i += 1
-                self.delay(i)
-
-    def publish(self, topic, msg, retain=False, qos=0):
-        while 1:
-            try:
-                return super().publish(topic, msg, retain, qos)
-            except OSError as e:
-                self.log(False, e)
-            self.reconnect()
-
-    def wait_msg(self):
-        while 1:
-            try:
-                return super().wait_msg()
-            except OSError as e:
-                self.log(False, e)
-            self.reconnect()
+                return super().wait_msg(socket_timeout)
+            except (OSError, simple2.MQTTException) as e:
+                self.conn_issue = e
---
+++ tests.py
@@ -0,0 +1,252 @@
+import utime
+from ubinascii import hexlify
+from umqtt.simple2 import MQTTException, pid_gen
+from umqtt.robust2 import MQTTClient as _MQTTClient
+
+
+def debug_print(data):
+    print('HEX: %s STR: /*' % hexlify(data).decode('ascii'), end='')
+    for i, d in enumerate(data):
+        if type(d) == str:
+            d = ord(d)
+        if d > 31 and d < 127:
+            print(chr(d), end='')
+        else:
+            print('.', end='')
+    print('*/')
+
+
+def debug_func_name(f_name):
+    print('FUNC: %s' % f_name)
+
+
+class MQTTClient(_MQTTClient):
+    DEBUG = 1
+    MAX_DBG_LEN = 80
+
+    def _read(self, n):
+        out = super()._read(n)
+        if type(out) == bytes:
+            print(' R(%3d) - ' % len(out), end='')
+            debug_print(out[:self.MAX_DBG_LEN])
+        else:
+            print(' R(---) - %s' % out)
+        return out
+
+    def _write(self, bytes_wr, *args, **kwargs):
+        print(' W(%3d,%3s) - ' % (len(bytes_wr), args[0] if args else '---'), end='')
+        debug_print(bytes_wr[:self.MAX_DBG_LEN])
+        return super()._write(bytes_wr, *args, **kwargs)
+
+    def connect(self, *a, **k):
+        debug_func_name('connect')
+        return super().connect(*a, **k)
+
+    def disconnect(self, *a, **k):
+        debug_func_name('disconnect')
+        return super().disconnect(*a, **k)
+
+    def ping(self, *a, **k):
+        debug_func_name('ping')
+        return super().ping(*a, **k)
+
+    def publish(self, *a, **k):
+        debug_func_name('publish')
+        return super().publish(*a, **k)
+
+    def subscribe(self, *a, **k):
+        debug_func_name('subscribe')
+        return super().subscribe(*a, **k)
+
+    def wait_msg(self, *a, **k):
+        debug_func_name('wait_msg')
+        return super().wait_msg(*a, **k)
+
+
+class TestMQTT:
+    def __init__(self, *args, **kwargs):
+        self.mqtt_client_args = (args, kwargs)
+        self.msg_id = args[0]
+        self.subsctiption_out = {}
+        self.status_out = {}
+        self.clients = {}
+        self.client = None
+
+    def init_mqtt_client(self, clientid_postfix='_1'):
+        args = list(self.mqtt_client_args[0][:])
+        kwargs = self.mqtt_client_args[1].copy()
+        if len(args) > 0:
+            args[0] += clientid_postfix
+        if 'client_id' in kwargs:
+            kwargs['client_id'] += clientid_postfix
+        print('MQTT connection args:', args, kwargs)
+        client = MQTTClient(*args, **kwargs)
+        client.set_callback(self.sub_cb_gen(clientid_postfix))
+        client.set_callback_status(self.stat_cb_gen(clientid_postfix))
+        self.subsctiption_out[clientid_postfix] = None
+        self.status_out[clientid_postfix] = None
+        self.clients[clientid_postfix] = client
+        return client
+
+    def sub_cb_gen(self, clientid_postfix='_1'):
+        def sub_cb(topic, msg, retained):
+            print('TOPIC: %s MSG: %s R: %s' % (topic, msg, retained))
+            self.subsctiption_out[clientid_postfix] = (topic, msg, retained)
+
+        return sub_cb
+
+    def stat_cb_gen(self, clientid_postfix='_1'):
+        def stat_cb(pid, status):
+            print('PID: %s STATUS: %d' % (pid, status))
+            self.status_out[clientid_postfix] = (pid, status)
+
+        return stat_cb
+
+    def get_subscription_out(self, timeout=5, clientid_postfix='_1'):
+        print('WAIT SUB: timeout=%d' % (timeout,))
+        client = self.clients[clientid_postfix]
+        for i in range(timeout):
+            client.check_msg()
+            if clientid_postfix in self.subsctiption_out and self.subsctiption_out[clientid_postfix] != None:
+                o = self.subsctiption_out[clientid_postfix]
+                self.subsctiption_out[clientid_postfix] = None
+                return o
+            utime.sleep(1)
+        raise Exception('timeout')
+
+    def get_status_out(self, timeout=5, pid=None, clientid_postfix='_1'):
+        print('WAIT STAT: timeout=%d pid=%s' % (timeout, pid))
+        client = self.clients[clientid_postfix]
+        for i in range(timeout + 1):
+            utime.sleep(1)
+            client.check_msg()
+            if clientid_postfix in self.status_out and self.status_out[clientid_postfix] != None:
+                o = self.status_out[clientid_postfix]
+                self.status_out[clientid_postfix] = None
+                if pid:
+                    if pid != o[0]:
+                        continue
+                return o
+        raise Exception('timeout')
+
+    def disable_net(self):
+        raise RuntimeError('Not implemented method')
+
+    def enable_net(self):
+        raise RuntimeError('Not implemented method')
+
+    def get_topic(self, test_name):
+        return '%s/umqtt.robust2/%s/' % (self.msg_id, test_name)
+
+    def run(self):
+        test_fails = []
+        tests = [
+            'test_publish_qos_0',
+            'test_publish_qos_1',
+            'test_subscribe',
+        ]
+        for test_name in tests:
+            if not self.run_test(test_name):
+                test_fails.append(test_name)
+        if test_fails:
+            print('\nTests fails: %s\n' % ', '.join(test_fails))
+        else:
+            print('All the tests were finished successfully!')
+
+    def run_test(self, test_name):
+        try:
+            if self.client:
+                self.client.disconnect()
+        except:
+            pass
+        self.client = self.init_mqtt_client()
+        self.subsctiption_out = {}
+        self.status_out = {}
+        test = getattr(self, test_name)
+        print('RUN [%s]' % test_name)
+        test_pass = True
+        self.enable_net()
+        try:
+            test(self.get_topic(test_name))
+        except Exception as e:
+            from sys import print_exception
+            print_exception(e)
+            test_pass = False
+        print('END [%s] %s\n' % (test_name, 'succes' if test_pass else 'FAIL'))
+        return test_pass
+
+    def test_publish_qos_0(self, topic):
+        self.client.connect()
+
+        self.client.sock.close()
+        self.client.publish(topic, 'test QoS 0', retain=True)
+        self.client.check_msg()
+        self.client.subscribe(topic + '#')
+        t, m, r = self.get_subscription_out()
+        assert t.decode('ascii') == topic
+        assert m.decode('ascii') == 'test QoS 0'
+
+        self.client.disconnect()
+
+    def test_publish_qos_1(self, topic):
+        self.client.connect()
+        rec_client = self.init_mqtt_client('_pub')
+
+        rec_client.connect()
+        rec_client.subscribe(topic + '#')
+
+        self.client.sock.close()
+        pid = self.client.publish(topic, 'test QoS 1', qos=1)
+
+        print('msg_to_:', self.client.msg_to_send, self.client.msg_to_confirm, self.client.rcv_pids)
+        assert self.client.msg_to_send == [(topic, 'test QoS 1', False, 1)]
+        assert self.client.msg_to_confirm == {}
+
+        self.client.check_msg()  # reconnect
+        assert self.client.send_queue()
+        print('msg_to_:', self.client.msg_to_send, self.client.msg_to_confirm, self.client.rcv_pids)
+        pid = list(self.client.rcv_pids.keys())[0]
+        assert self.client.msg_to_send == []
+        assert self.client.msg_to_confirm == {(topic, 'test QoS 1', False, 1): [pid]}
+
+        self.client.check_msg()  # get a confirmation
+        out_pid, status = self.get_status_out(pid=pid)
+        assert status == 1
+
+        print('msg_to_:', self.client.msg_to_send, self.client.msg_to_confirm, self.client.rcv_pids)
+        assert self.client.msg_to_send == []
+        assert self.client.msg_to_confirm == {}
+
+        self.client.disconnect()
+
+    def test_subscribe(self, topic):
+        self.client.connect()
+
+        self.client.sock.close()
+        pid = self.client.subscribe(topic + '#')
+
+        print(self.client.sub_to_send, self.client.sub_to_confirm, self.client.rcv_pids, 2)
+        assert self.client.sub_to_send == [(topic + '#', 0)]
+        assert self.client.sub_to_confirm == {}
+
+        self.client.check_msg()  # reconnect
+
+        print(self.client.sub_to_send, self.client.sub_to_confirm, self.client.rcv_pids, 3)
+        assert self.client.sub_to_send == [(topic + '#', 0)]
+        assert self.client.sub_to_confirm == {}
+
+        assert self.client.send_queue()  # resubscribe
+
+        print(self.client.sub_to_send, self.client.sub_to_confirm, self.client.rcv_pids, 4)
+        pid = list(self.client.rcv_pids.keys())[0]
+        assert self.client.sub_to_send == []
+        assert self.client.sub_to_confirm == {(topic + '#', 0): [pid]}
+
+        out_pid, status = self.get_status_out(pid=pid)
+
+        print(self.client.sub_to_send, self.client.sub_to_confirm, self.client.rcv_pids, 5)
+        assert status == 1
+        assert self.client.sub_to_send == []
+        assert self.client.sub_to_confirm == {}
+
+        self.client.disconnect()
